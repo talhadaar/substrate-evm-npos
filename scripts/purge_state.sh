9b3da89a11162d21d4811181811e5fb0b1042df3
Muhammad Talha Dar <talha.dar@invozone.com>
2022-08-01 19:14:29 +0500

    state purge scripts added

---
+++ scripts/purge_state.sh
@@ -0,0 +1,9 @@
+## declare an array variable
+declare -a arr=("alice" "bob" "charlie" "dave" "eve" "ferdie")
+
+## now loop through the above array
+for i in "${arr[@]}"
+do
+   echo "$i"
+   ./target/release/kories-node purge-chain --base-path /tmp/$i --chain local
+done
